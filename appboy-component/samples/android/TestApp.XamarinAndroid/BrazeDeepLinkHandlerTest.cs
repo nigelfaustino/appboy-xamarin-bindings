7bbef5ea7a6673cd0df12f188a941451194d1fd1
Nigel Faustino <nigelfaustino@gmail.com>
2021-09-14 12:23:48 -0700

    * BrazeDeepLinkHandlerTest.cs: Add custom AppboyNavigator
    * MainApplication.cs:
    * TestApp.XamarinAndroid.csproj:

---
+++ appboy-component/samples/android/TestApp.XamarinAndroid/BrazeDeepLinkHandlerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.Content;
+using Com.Appboy.UI;
+using Com.Appboy.UI.Actions;
+
+namespace com.appboy.xamarinsample
+{
+  public class BrazeDeepLinkHandlerTest : AppboyNavigator
+  {
+    public override void GotoUri (Context context, UriAction uriAction)
+    {
+      String uri = uriAction.Uri.ToString ();
+      Console.WriteLine ("Received attempt to navigate to: " + uri);
+    }
+  }
+}
